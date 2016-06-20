d3e0b3ccb113bd50b8c8a07845ca0063264dd2da
damonmcc <damonmcc1391@gmail.com>
2016-06-20 11:15:29 -0400

    initial project version

---
+++ cameraBEES.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera, Color
+from time import sleep
+from datetime import datetime as dt
+
+camera = PiCamera()
+camera.rotation = 180
+camera.resolution = (1920, 1080)
+camera.framerate = 15
+camera.annotate_background = Color('blue')
+camera.annotate_foreground = Color('yellow')
+timestamp = dt.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+
+camera.start_preview(alpha=200)
+camera.start_recording(______)
+for i in range(100):
+    camera.annotate_text = "Hello bees! %s" % i
+    camera.brightness = i
+    sleep(0.1)
+camera.stop_recording()
+camera.stop_preview()
