cc80af9f7d795c991bda305a1c2bdcedfbb3e461
damonmcc <damonmcc1391@gmail.com>
2016-06-21 12:32:46 -0400

    Edited recording scriot

--- cameraBEES.py
+++ cameraBEES.py
@@ -1,8 +1,9 @@
 from picamera import PiCamera, Color
 from time import sleep
 from datetime import datetime as dt
-from subprocess import check_call
+import subprocess
 import shutil
+import sys
 
 camera = PiCamera()
 camera.rotation = 180
@@ -11,9 +12,10 @@ camera.framerate = 15
 camera.annotate_background = Color('blue')
 camera.annotate_foreground = Color('yellow')
 timestamp = dt.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
-convCommand = '-add {0}.h264 {1}.mp4'.format(timestamp,timestamp)
+convCommand = '{0}.h264 {1}.mp4'.format(timestamp,timestamp)
 srcroot = '/home/pi/Videos/%s' % timestamp
 dstroot = '/media/pi/BEEVIDS'
+scriptroot = '/home/pi/Documents/Python 3 Projects/Bee Camera'
 
 camera.start_preview(alpha=200)
 camera.start_recording(srcroot)
@@ -23,6 +25,13 @@ for i in range(6):
     sleep(0.5)
 camera.wait_recording(3)
 camera.stop_recording()
-shutil.copy(srcroot, dstroot)
-check_call(["MP4Box",convCommand], shell=True, cwd=dstroot)
+shutil.copy(srcroot, scriptroot)
+p = subprocess.Popen("ls", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=dstroot)
+p_status = p.wait()
+for line in iter(p.stdout.readline, b''):
+    print (line)
+#(output, err) = p.communicate()
+#p_status = p.wait()
+#print ("*** Running ls ***\n", output)
+print ("*** Command exit status/return code: ", p_status)
 camera.stop_preview()
