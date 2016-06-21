7affbd7d6af8b68df3c00fa66aee9bbdd9d26599
damonmcc <damonmcc1391@gmail.com>
2016-06-20 22:43:48 -0400

    Edited recording scriot

--- cameraBEES.py
+++ cameraBEES.py
@@ -1,7 +1,7 @@
 from picamera import PiCamera, Color
 from time import sleep
 from datetime import datetime as dt
-from subprocess import call
+from subprocess import check_call
 import shutil
 
 camera = PiCamera()
@@ -11,19 +11,18 @@ camera.framerate = 15
 camera.annotate_background = Color('blue')
 camera.annotate_foreground = Color('yellow')
 timestamp = dt.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
-convCommand = "MP4Box -add {0}.h264 {1}.mp4".format(timestamp,timestamp)
+convCommand = '-add {0}.h264 {1}.mp4'.format(timestamp,timestamp)
 srcroot = '/home/pi/Videos/%s' % timestamp
 dstroot = '/media/pi/BEEVIDS'
 
 camera.start_preview(alpha=200)
 camera.start_recording(srcroot)
-for i in range(20):
+for i in range(6):
     timeElapS = float(i)*0.5
     camera.annotate_text = "Elapsed: 0:00:%s" % timeElapS
     sleep(0.5)
 camera.wait_recording(3)
 camera.stop_recording()
 shutil.copy(srcroot, dstroot)
-call("cd", "dstroot")
-call([convCommand])
+check_call(["MP4Box",convCommand], shell=True, cwd=dstroot)
 camera.stop_preview()
