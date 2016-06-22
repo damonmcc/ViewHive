c773bc520f4f834383188de7ccb83a34cd0ce6af
damonmcc <damonmcc1391@gmail.com>
2016-06-21 22:44:58 -0400

    Added h264 file deletion

--- cameraBEES.py
+++ cameraBEES.py
@@ -4,6 +4,14 @@ from datetime import datetime as dt
 import subprocess
 import shutil
 import sys
+import os, errno
+def silentremove(filename):
+    try:
+        os.remove(filename)
+        print("*** Deleted: ", filename)
+    except OSError as e:
+        if e.errno != errno.ENOENT: # no such file or directory
+            raise # re-raise exception if a different error occured
 
 camera = PiCamera()
 camera.rotation = 180
@@ -15,7 +23,7 @@ timestamp = dt.now().strftime("%Y-%m-%d_%H.%M.%S")
 srcfile = '%s.h264' % timestamp
 convCommand = 'MP4Box -add {0}.h264 {1}.mp4'.format(timestamp,timestamp)
 srcroot = '/home/pi/Videos/%s' % srcfile
-dstroot = '/media/pi/BEEVIDS'
+dstroot = '/media/pi/BEEVIDS/'
 scriptroot = '/home/pi/Documents/Python 3 Projects/Bee Camera'
 
 camera.start_preview(alpha=200)
@@ -28,19 +36,18 @@ camera.wait_recording(3)
 camera.stop_recording()
 shutil.copy(srcroot, dstroot)
 print(convCommand)
-conv = subprocess.call(convCommand, shell=True,
+conv = subprocess.Popen(convCommand, shell=True,
                     cwd=dstroot)
 conv_status = conv.wait()
 #for line in iter(conv.stdout.readline, b''):
 #    print (line)
-    
+silentremove(srcroot)
+silentremove("{0}{1}.h264".format(dstroot,timestamp))
 p = subprocess.Popen("ls", shell=True,
                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                      cwd=dstroot)
 p_status = p.wait()
 for line in iter(p.stdout.readline, b''):
     print (line)
-
-print ("*** ls exit status/return code: ", p_status)
 print ("*** conv exit status/return code: ", conv_status)
 camera.stop_preview()
