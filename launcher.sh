d23ff3aee5807352ce0a3cdfca7032baadc623ff
damonmcc <damonmcc1391@gmail.com>
2016-06-27 16:21:57 -0400

    Corrected launcher navigation and added to crontab

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/Documents/Python\3\Projects/Bee\Camera
+cd /home/pi/Documents/Python\ 3\ Projects/Bee\ Camera
 sudo python cameraBEES.py
 cd /
