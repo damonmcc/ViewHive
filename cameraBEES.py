52aa14567077d824e0dca6790cc849b783ccc23a
damonmcc <damonmcc1391@gmail.com>
2016-07-07 16:04:26 -0400

    Moved launcher script to root dir and appended rc.local

--- cameraBEES.py
+++ cameraBEES.py
@@ -8,11 +8,13 @@ import os, errno
 def silentremove(filename):
     try:
         os.remove(filename)
-        print("*** Deleted: ", filename)
+        print ("*** Deleted: ", filename)
     except OSError as e:
         if e.errno != errno.ENOENT: # no such file or directory
             raise # re-raise exception if a different error occured
-
+#def savetoUSB(drivename):
+#    try:
+        
 camera = PiCamera()
 camera.rotation = 180
 camera.resolution = (1920, 1080)
@@ -21,20 +23,24 @@ camera.framerate = 30
 #camera.framerate = 49
 camera.annotate_background = Color('grey')
 camera.annotate_foreground = Color('purple')
+sleep(15)
 timestamp = dt.now().strftime("%Y-%m-%d_%H.%M.%S")
 srcfile = '%s.h264' % timestamp
 convCommand = 'MP4Box -add {0}.h264 {1}.mp4'.format(timestamp,timestamp)
 srcroot = '/home/pi/Videos/%s' % srcfile
 dstroot = '/media/pi/BEEVIDS/'
 codetroot = '/home/pi/Documents/Python 3 Projects/Bee Camera'
-recRes = 0.01 # resolution of elapsed time counter
-
+recRes = 0.01 # resolution of elapsed time counter (seconds)
 recPeriod = 3 # Seconds to record
-
 camera.start_preview(alpha=200)
-print('*** Active on %s***' % timestamp)
+
+
+# First copy custom schedule to wittyPi folders
+print('*** Active on %s***\n' % timestamp)
+os.system("sudo cp -v ./HVScript0.wpi /home//wittyPi/schedules/HVScript0.wpi")
+os.system("sudo cp -v ./HVScript0.wpi /home/wittyPi/schedule.wpi")
 if (os.path.exists(dstroot)==False):
-    print("**! No USB Stoarge named BEEHIVE !**")
+    print("**! No USB Stoarge named BEEVIDS !**")
     quit()
 
 camera.start_recording(srcroot, format='h264')
@@ -63,11 +69,10 @@ for line in iter(p.stdout.readline, b''):
 if conv_status==0:
     print ("*** Conversion complete ***")
 else:
-    print("**! Conversion FAILED !**")
+    print ("**! Conversion FAILED !**")
 camera.stop_preview()
 camera.close()
 
-os.system("sudo cp -v ./HVScript0.wpi ~/wittyPi/schedule.wpi")
 quit()
 #
 #
--- cameraPreview.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-
-camera.rotation = 180
-camera.start_preview(alpha=100)
-sleep(10)
-camera.stop_preview()
