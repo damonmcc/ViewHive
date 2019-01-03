fd67ca84e68223bebcf451d1f38f52cde3d5814e
Damon McCullough <damonmcc1391@gmail.com>
2019-01-02 22:41:40 -0500

    Logging update function

--- viewhive/ViewHiveUtil.py
+++ viewhive/ViewHiveUtil.py
@@ -826,12 +826,12 @@ class Display(object):
                        "UPDATING...", font=self.fontBig, fill=0)
         self.update()
         loggerVH.info("Clearing changes with: git reset --hard origin/master")
-        subprocess.check_call('git reset --hard origin/master',
-                              shell=True, cwd=TGT_DIR)
+        loggerVH.info(subprocess.check_call('git reset --hard origin/master',
+                              shell=True, cwd=TGT_DIR))
         loggerVH.info("Updating with: git pull")
         # run('sudo %s' % UPDATE_CMD)
-        subprocess.check_call('git pull',
-                              shell=True, cwd=TGT_DIR)
+        loggerVH.info(subprocess.check_call('git pull',
+                              shell=True, cwd=TGT_DIR))
         # subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-U',
         #                        'git+https://github.com/damonmcc/ViewHive#egg=ViewHive'])
         loggerVH.info("Update complete, restarting program")
